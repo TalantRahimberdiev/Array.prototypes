4054c3d405701d775f80e25ccdd6e19263ccac20
TalantRahimberdiev <76486068+TalantRahimberdiev@users.noreply.github.com>
2021-07-30 13:45:12 +0600

    Update glavka.js

--- glavka.js
+++ glavka.js
@@ -3,17 +3,7 @@
 Array.prototype.myEach = function(callback) {
     for (var i = 0; i < this.length; i++)
         callback(this[i], i, this);
-};
-
-//tests
-var arr = ['biggy smalls', 'bif tannin', 'boo radley', 'hans gruber'];
-arr.myEach(function(word) {
-    console.log(word);
-});
-//biggy smalls
-//bif tannin
-//boo radley
-//hans gruber
+}
 
 /*****************NATIVE MAP*************************/
 
@@ -22,22 +12,7 @@ Array.prototype.myMap = function(callback) {
     for (var i = 0; i < this.length; i++)
         arr.push(callback(this[i], i, this));
     return arr;
-};
-
-//tests
-var arrs = ['dic tanin', 'boo radley', 'hans gruber'];
-var numbers2 = [1, 4, 9];
-
-var goodT = arrs.myMap(function(n) {
-    return n;
-});
-
-var squareRoot = numbers2.myMap(function(num) {
-    return Math.sqrt(num);
-});
-
-console.log(goodT); // [ 'dic tanin', 'boo radley', 'hans gruber' ]
-console.log(squareRoot); // [ 1, 2, 3 ]
+}
 
 /*****************NATIVE FILTER*************************/
 
@@ -48,14 +23,7 @@ Array.prototype.myFilter = function(callback, context) {
             arr.push(this[i]);
     }
     return arr;
-};
-
-//tests
-var numbers = [1, 20, 30, 80, 2, 9, 3];
-var newNum = numbers.myFilter(function(n) {
-    return n >= 10;
-});
-console.log(newNum); // [ 20, 30, 80 ]
+}
 
 /*****************NATIVE REDUCE*************************/
 
@@ -71,23 +39,6 @@ Array.prototype.myReduce = function(callback, initialVal) {
     return accumulator;
 };
 
-//tests
-var numbers3 = [20, 20, 2, 3];
-var total = numbers3.myReduce(function(a, b) {
-    return a + b;
-}, 10);
-console.log(total); // 55
-
-var flattened = [
-    [0, 1],
-    [2, 3],
-    [4, 5]
-].reduce(function(a, b) {
-    return a.concat(b);
-});
-console.log(flattened); //[ 0, 1, 2, 3, 4, 5 ]
-
-
 /*****************NATIVE EVERY*************************/
 
 Array.prototype.myEvery = function(callback, context) {
@@ -96,24 +47,7 @@ Array.prototype.myEvery = function(callback, context) {
             return false;
     }
     return true;
-};
-
-//tests
-var passed = [12, 5, 8, 130, 44].myEvery(function(element) {
-    return (element >= 10);
-});
-console.log(passed); // false
-passed = [12, 54, 18, 130, 44].myEvery(function(element) {
-    return (element >= 10);
-});
-console.log(passed); // true
-
-passed = [12, 54, 18, 130, 44].myEvery(function(element) {
-    return (element >= 13);
-});
-console.log(passed); // false
-
-
+}
 
 /*****************NATIVE SOME*************************/
 
@@ -124,14 +58,3 @@ Array.prototype.mySome = function(callback, context) {
     }
     return false;
 };
-
-//tests
-var passed = [12, 5, 8, 130, 44].mySome(function(element) {
-    return (element >= 200);
-});
-console.log('some: ' + passed); //some: false
-
-var passed = [12, 5, 8, 130, 44].mySome(function(element) {
-    return (element >= 100);
-});
-console.log('some: ' + passed); //some: true
